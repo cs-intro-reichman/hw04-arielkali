82f252f3efbf86f2dadd36c1899176ee726f910d
Ariel Kalish <ariel.kalish@post.runi.ac.il>
2025-11-20 14:17:17 +0200

    fixed bugs in primes to include biggest number

--- Primes.java
+++ Primes.java
@@ -1,7 +1,7 @@
 public class Primes {
     public static void main(String[] args) {
         int largestNum = Integer.parseInt(args[0]);
-        boolean[] arr = new boolean[largestNum];
+        boolean[] arr = new boolean[largestNum+1];
         for (int i = 2; i < arr.length; i++) {
             arr[i] = true;
         }
