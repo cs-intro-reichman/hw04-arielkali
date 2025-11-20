5a9a29af858596f071d8b1ff5d03ddc1c2951f48
Ariel Kalish <ariel.kalish@post.runi.ac.il>
2025-11-20 14:04:18 +0200

    primes first draft

--- Primes.java
+++ Primes.java
@@ -1,5 +1,35 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int largestNum = Integer.parseInt(args[0]);
+        boolean[] arr = new boolean[largestNum];
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = true;
+        }
+        int p = 2;
+        int i = 3;
+        while (p < arr.length) {
+            while (i < arr.length) {
+                if (i % p == 0) {
+                    arr[i] = false;
+                }
+                i++;
+            }
+            p++;
+            while (arr[p] == false) {
+                p++;
+            }
+        }
+
+        System.out.println("Prime numbers up to " +largestNum+":");
+        int count_prime = 0;
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j]) {
+                System.out.println(arr[j]);
+                count_prime++;
+            }
+        }
+
+        double percentage = ((double) count_prime / (double) largestNum) * 100;
+        System.out.println("There are "+ count_prime +" primes between 2 and "+ largestNum +" ("+ percentage +"% are primes)");
     }
 }
